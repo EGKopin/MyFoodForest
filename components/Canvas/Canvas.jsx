320df2d70d434b8fdb1ae5b3b20d5b37229fcd76
Etana Kopin <claws.33@gmail.com>
2023-06-13 16:45:07 -0600

    commented out canvas - troubleshooting

--- components/Canvas/Canvas.jsx
+++ components/Canvas/Canvas.jsx
@@ -1,20 +1,20 @@
-import {fabric} from "fabric";
-import { useRef, useEffect, useState } from "react";
+// import {fabric} from "fabric";
+// import { useRef, useEffect, useState } from "react";
 
-const Canvas = () => {
-  const canvasRef = useRef(null); //this creates the relationship between the canvas and the ref
+// const Canvas = () => {
+//   const canvasRef = useRef(null); //this creates the relationship between the canvas and the ref
 
-  useEffect(() => {
-    const canvas = new fabric.Canvas(canvasRef.current,{
-      backgroundColor: "red",
-    })
-  }, [])
+//   useEffect(() => {
+//     const canvas = new fabric.Canvas(canvasRef.current,{
+//       backgroundColor: "red",
+//     })
+//   }, [])
 
-  return (
-    <div>
-      <canvas ref={canvasRef}></canvas>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <canvas ref={canvasRef}></canvas>
+//     </div>
+//   )
+// }
 
-export default Canvas;
+// export default Canvas;
